940b208b4f45a9255c0242dac5a7b751211b1e89
kola-samuel <samuel.dev.dba@gmail.com>
2019-08-14 11:44:09 +0100

    Handle Dlock Re run code changes

---
+++ src/SFA.DAS.Payments.Model.Core/Audit/EarningEventModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Payments.Model.Core.Entities;
+
+namespace SFA.DAS.Payments.Model.Core.Audit
+{
+    public class EarningEventModel: PaymentsEventModel
+    {
+        public ContractType ContractType { get; set; }
+        public string AgreementId { get; set; }
+        public int LearningAimSequenceNumber { get; set; }
+        public List<EarningEventPeriodModel> Periods { get; set; }
+        public List<EarningEventPriceEpisodeModel> PriceEpisodes { get; set; }
+    } 
+}
