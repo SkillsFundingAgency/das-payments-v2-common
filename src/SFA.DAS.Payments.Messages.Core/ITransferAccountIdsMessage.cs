c001787a31aaf10b161fcb66444dc286790ffcf7
Liam Wright <mail@liam-wright.co.uk>
2024-11-07 09:28:13 +0000

    PV2-3295 add core types to support funding platform

---
+++ src/SFA.DAS.Payments.Messages.Core/ITransferAccountIdsMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Payments.Messages.Core
+{
+    public interface ITransferAccountIdsMessage
+    {
+        long? AccountId { get; set; }
+        long? TransferSenderAccountId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Payments.Model.Core/Entities/FundingPlatformType.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Payments.Model.Core.Entities
+{
+    public enum FundingPlatformType : byte
+    {
+        SubmitLearnerData = 1,
+        DigitalApprenticeshipService = 2
+    }
+}
