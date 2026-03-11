ef846e74b902ffcb45bfa4984bfe438244098a8a
syed-wali <6061008+syed-wali@users.noreply.github.com>
2026-03-11 11:56:54 +0000

    PV2-3835 - learning type enum

---
+++ src/SFA.DAS.Payments.Model.Core/Entities/LearningType.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Payments.Model.Core.Entities
+{
+    public enum LearningType : byte
+    {
+        Apprenticeship = 1,
+        FoundationApprenticeship = 2,
+        ApprenticeshipUnit = 3  
+    }
+}
